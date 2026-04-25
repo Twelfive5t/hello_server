ced95fc5566c4d72afee46c62184d95ef7269416
Twelfive5t <liuyijieliuyijie@gmail.com>
2026-04-25 09:08:23 +0000

    chore(conan): 引入 conan 包管理，添加 spdlog 依赖
    - 新增 conanfile.py，依赖 spdlog/1.13.0
    - 新增 conanfile/ 目录，提供各平台 (x86_64/aarch64) 的 debug/release 构建配置
    - CMakePresets.json: 为 Debug/Release 预设添加 CMAKE_TOOLCHAIN_FILE 指向 conan 生成的工具链
    - build.sh: 添加 conan install 步骤，检测工具链文件已存在则跳过

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -e
 
 # 如果没有传入参数，使用默认值 'Server_Release'
 PRESET="${1:-Server_Release}"
@@ -9,6 +9,29 @@ if [[ "$PRESET" == *"Debug"* ]]; then
     BUILD_TYPE="Debug"
 fi
 BUILD_DIR="build/$BUILD_TYPE/"
+
+# 根据 PRESET 配置并安装依赖
+CONAN_PROFILE=""  # Conan 配置文件
+ARCH=$(uname -m)
+
+case "$PRESET" in
+  Server_Debug)
+    CONAN_PROFILE="./conanfile/linux_${ARCH}_debug"
+    ;;
+  Server_Release)
+    CONAN_PROFILE="./conanfile/linux_${ARCH}_release"
+    ;;
+  *)
+    echo "未知的 PRESET: $PRESET, 跳过特殊操作"
+    ;;
+esac
+
+if [ ! -f "$BUILD_DIR/generators/conan_toolchain.cmake" ]; then
+  echo "Running conan install..."
+  conan install . --profile:all="$CONAN_PROFILE" --build=missing -c tools.system.package_manager:mode=install
+else
+  echo "Skipping conan install as '$BUILD_DIR/conan_toolchain.cmake' already exists."
+fi
 
 cmake --preset "$PRESET"
 cmake --build --preset "$PRESET" -j$(nproc)
---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from os import path
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    # The requirements() method is used to specify the dependencies of a package.
+    # https://docs.conan.io/2.0/reference/conanfile/methods/requirements.html
+    def requirements(self):
+        self.requires("spdlog/1.13.0")
+
+    # The build_requirements() method is used to define tool_requires and test_requires:
+    # https://docs.conan.io/2/reference/conanfile/methods/build_requirements.html
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.22 <3.23]")
+
+    # The cmake_layout() sets the folders and cpp attributes to follow the structure of a typical CMake project.
+    # https://docs.conan.io/2.0/reference/tools/cmake/cmake_layout.html
+    def layout(self):
+        cmake_layout(self)
+        self.folders.build  = path.join("build", str(self.settings.build_type))
+        self.folders.generators = path.join(self.folders.build, "generators")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
+
+    # The build() method is used to define the build from source of the package.
+    # https://docs.conan.io/2.0/reference/conanfile/methods/build.html
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    # The package() method is in charge of copying files from the source_folder and the temporary build_folder to the package_folder
+    ## https://docs.conan.io/2/reference/conanfile/methods/package.html#
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
