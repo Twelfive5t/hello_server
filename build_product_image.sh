6265edae9de9a07f933fc28bd42bdfafe319c3f4
Twelfive5t <liuyijieliuyijie@gmail.com>
2026-05-04 01:09:27 +0800

    feat(observability): 使用 Grafana Alloy、Tempo 和 Grafana 替换 Jaeger
    新增 Alloy、Tempo、Grafana 配置与 compose 服务，并在产品打包时同步复制观测配置

--- build_product_image.sh
+++ build_product_image.sh
@@ -7,6 +7,7 @@ cp start_service.sh products/bin$SERVER_VERSION/start_service.sh
 cp stop_service.sh products/bin$SERVER_VERSION/stop_service.sh
 cp package/Dockerfile products/bin$SERVER_VERSION/Dockerfile
 cp docker-compose.yaml products/bin$SERVER_VERSION/docker-compose.yaml
+cp -r config products/bin$SERVER_VERSION/config
 
 cd products/bin$SERVER_VERSION
 
